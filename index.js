32bd3e92efd8e3633873208d04bbc515e333953d
petermunene <peter.munene@student.moringaschool.com>
2025-03-06 08:36:44 +0300

    strings

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Peter Munene'
+let welcomeMessage = `Welcome to Flatbook, ${currentUser } !`;
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, "+ currentUser.toUpperCase() + "!";
+let shortGreeting = "Welcome, "+ currentUser.slice(0,6) + "!";
